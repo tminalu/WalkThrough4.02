2f3ab7738658bfc93b5b0521be7668b1cadb0810
tminalu <minalutem@gmail.com>
2019-12-12 15:26:19 -0500

    1

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure (HttpSecurity http) throws Exception{
         http
-                            .authorizeRequests().anyRequest().authenticated().and().formLogin();
+                            .authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
 
     }
     @Override
